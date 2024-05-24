f6222462a61961fb1c5ac0d397dcdce07369e57d
Abhay3003 <abhaygupta.3003@gmail.com>
2024-05-24 15:46:18 +1000

    initial project setup with entity

---
+++ src/main/java/com/appsdeveloperblog/UserService/RemoteUserStorageService/RemoteUserStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.UserService.RemoteUserStorageService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteUserStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteUserStorageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appsdeveloperblog/UserService/RemoteUserStorageService/RemoteUserStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.UserService.RemoteUserStorageService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteUserStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
